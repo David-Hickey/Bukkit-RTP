ae0dd3d8909537cd3a4e670e7f668b2f8ebe30ee
David Hickey <44777933+David-Hickey@users.noreply.github.com>
2019-06-22 20:59:55 +0100

    Dramatic improvements to help commands

--- src/main/java/davidhickey/bukkit_rtp/command/SuperCommand.java
+++ src/main/java/davidhickey/bukkit_rtp/command/SuperCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.ChatColor;
 
 public class SuperCommand {
 
@@ -45,18 +46,22 @@ public class SuperCommand {
     public void addHelpCommand() {
         addSubCommand(new SubCommand(plugin, this.pluginCommand, new SubCommandExecutor(){
             public boolean execute(SubCommand command, CommandSender sender, String superAlias, String alias, String[] args) {
-                printHelp(sender);
+                printHelp(sender, superAlias);
                 return true;
             }
-        }, "help", "display help", null, "help", "h"));
+        }, "help", "display help", null, "/<command> help", "h"));
     }
 
-    protected void printHelp(CommandSender sender) {
+    protected void printHelp(CommandSender sender, String superAlias) {
+        sender.sendMessage(ChatColor.GRAY + "Help for command " + ChatColor.GOLD + "/" + superAlias + ChatColor.GRAY + ":");
         for (SubCommand command : commands) {
-            sender.sendMessage(command.getName() + ":");
-            sender.sendMessage("    " + command.getDescription());
-            sender.sendMessage("    " + command.getUsage());
-            sender.sendMessage("    " + command.getPermission());
+            sender.sendMessage(" " + ChatColor.GOLD + "/" + superAlias + " " + command.getName() + ChatColor.GRAY + ":");
+            sender.sendMessage("  " + ChatColor.GRAY + "Description: " + ChatColor.GOLD + command.getDescription());
+            sender.sendMessage("  " + ChatColor.GRAY + "Usage: " + ChatColor.GOLD + command.getUsage(superAlias));
+            if (command.getAliases().length > 0) {
+                sender.sendMessage("  " + ChatColor.GRAY + "Aliases: " + ChatColor.GOLD
+                    + String.join(ChatColor.GRAY + ", " + ChatColor.GOLD, command.getAliases()));
+            }
             sender.sendMessage("");
         }
     }
@@ -70,7 +75,7 @@ public class SuperCommand {
             if (this.defaultSubCommand != null) {
                 this.defaultSubCommand.execute(sender, alias, this.defaultSubCommand.getName(), new String[0]);
             } else {
-                printHelp(sender);
+                printHelp(sender, alias);
             }
 
             return true;
@@ -80,6 +85,11 @@ public class SuperCommand {
         String subCommandAlias = args[0];
 
         SubCommand toExecute = this.getByNameOrAlias(subCommandAlias);
+
+        if (toExecute == null) {
+            sender.sendMessage(ChatColor.RED + "Invalid subcommand - try " + ChatColor.DARK_RED + "/" + alias + " help" + ChatColor.RED + ".");
+            return true;
+        }
 
         return toExecute.execute(sender, alias, subCommandAlias, subCommandArgs);
     }
