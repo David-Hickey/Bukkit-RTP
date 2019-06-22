e78e8a874579ecd420c118bfb3ca5fcfeb9e5c8b
David Hickey <44777933+David-Hickey@users.noreply.github.com>
2019-06-22 13:14:54 +0100

    Added missing imports Changed hasKey to containsKey

--- src/davidhickey/bukkit_rtp/RTPPlugin.java
+++ src/davidhickey/bukkit_rtp/RTPPlugin.java
@@ -2,6 +2,8 @@ package davidhickey.bukkit_rtp;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
+import davidhickey.bukkit_rtp.WorldInfo;
 import java.util.HashMap;
 
 public class RTPPlugin extends JavaPlugin {
@@ -10,7 +12,7 @@ public class RTPPlugin extends JavaPlugin {
     private HashMap<World, WorldInfo> enabledWorldInfo;
 
     private boolean enabledForWorld(World w) {
-        return enabledWorldInfo.hasKey(w);
+        return enabledWorldInfo.containsKey(w);
     }
 
     @Override
