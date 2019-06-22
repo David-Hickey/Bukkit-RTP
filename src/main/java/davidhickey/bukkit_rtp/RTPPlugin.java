969df39e27e10fa2a9e7cf41e4941bff79f6553d
David Hickey <44777933+David-Hickey@users.noreply.github.com>
2019-06-22 20:30:55 +0100

    Implemented bukkit command registration logic

--- src/main/java/davidhickey/bukkit_rtp/RTPPlugin.java
+++ src/main/java/davidhickey/bukkit_rtp/RTPPlugin.java
@@ -2,7 +2,9 @@ package davidhickey.bukkit_rtp;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.entity.Player;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 
 import davidhickey.bukkit_rtp.storage.*;
 import davidhickey.bukkit_rtp.command.*;
@@ -10,6 +12,7 @@ import davidhickey.bukkit_rtp.command.*;
 public class RTPPlugin extends JavaPlugin {
 
     private RTPDataStore storedData;
+    private SuperCommand rtpCommand;
 
     @Override
     public void onEnable() {
@@ -18,9 +21,18 @@ public class RTPPlugin extends JavaPlugin {
 
         storedData = new RTPDataStore(getConfig(), getLogger(), this.getServer().getWorlds());
 
-        PluginCommand rtpCommand = getCommand("rtp");
-        rtpCommand.setTabCompleter(new RTPCommandTabCompleter());
-        rtpCommand.setExecutor(new RTPCommandExecutor());
+        PluginCommand bukkitRtpCommand = getCommand("rtp");
+
+        rtpCommand = new SuperCommand(this, bukkitRtpCommand);
+        rtpCommand.addSubCommand(new RTPTeleportCommand(this));
+        rtpCommand.addHelpCommand();
+
+        bukkitRtpCommand.setTabCompleter(new RTPCommandTabCompleter());
+        bukkitRtpCommand.setExecutor(new CommandExecutor(){
+            public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
+                return rtpCommand.executeSuperCommand(sender, command, alias, args);
+            }
+        });
     }
 
     public RTPDataStore getDataStorage() {
