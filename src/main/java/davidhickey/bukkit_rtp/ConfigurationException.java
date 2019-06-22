19251af17b66bbac2eebad715e9f1d98a0c55bf0
David Hickey <44777933+David-Hickey@users.noreply.github.com>
2019-06-22 17:36:27 +0100

    Added serialVersionUID

--- src/main/java/davidhickey/bukkit_rtp/ConfigurationException.java
+++ src/main/java/davidhickey/bukkit_rtp/ConfigurationException.java
@@ -2,6 +2,8 @@ package davidhickey.bukkit_rtp;
 
 public class ConfigurationException extends Exception {
 
+    private static final long serialVersionUID = 42L;
+
     public ConfigurationException(String msg) {
         super(msg);
     }
