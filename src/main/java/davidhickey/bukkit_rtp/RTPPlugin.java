92458f46ba4ed1de50aacc78366b626d5dd7d9cb
David Hickey <44777933+David-Hickey@users.noreply.github.com>
2019-06-22 14:10:21 +0100

    Enabled tab completion and command execution

--- src/main/java/davidhickey/bukkit_rtp/RTPPlugin.java
+++ src/main/java/davidhickey/bukkit_rtp/RTPPlugin.java
@@ -1,9 +1,9 @@
 package davidhickey.bukkit_rtp;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-import davidhickey.bukkit_rtp.WorldInfo;
 import java.util.HashMap;
 
 public class RTPPlugin extends JavaPlugin {
@@ -22,5 +22,10 @@ public class RTPPlugin extends JavaPlugin {
 
         lastUsed = new HashMap<>();
         enabledWorldInfo = new HashMap<>();
+
+
+        PluginCommand rtpCommand = getCommand("rtp");
+        rtpCommand.setTabCompleter(new RTPCommandTabCompleter());
+        rtpCommand.setExecutor(new RTPCommandExecutor());
     }
 }
