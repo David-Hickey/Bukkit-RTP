2b1a11a6e4af589e54df521ba5b0cc62d37624c9
David Hickey <44777933+David-Hickey@users.noreply.github.com>
2019-06-22 13:04:36 +0100

    Added WorldInfo class

---
+++ src/davidhickey/bukkit_rtp/WorldInfo.java
@@ -0,0 +1,17 @@
+package davidhickey.bukkit_rtp;
+
+import org.bukkit.Location;
+
+public class WorldInfo {
+
+    private final int radius;
+    private final Location centre;
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public Location getCentre() {
+        return centre;
+    }
+}
